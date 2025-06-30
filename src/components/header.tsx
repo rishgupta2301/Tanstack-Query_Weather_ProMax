22727cc70002e2fa0d47d0df5c9b6ba4561ac862
Rishabh Gupta <guptarishabh1122@gmail.com>
2025-06-30 13:26:54 +0530

    added logos for dark and light mode along with the animation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,23 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { useTheme } from "../context/theme-provider";
+import { Moon, Sun } from "lucide-react";
 
 const Header = () => {
-  return (
-    <header>
-        <div>
-            <Link to={'/'} >
-            <img 
-            src="/logo.png" alt="WeatherPro logo" 
-            className='h-14'
-            />
-            </Link>
-            <div>
-                {/* search */}
-                {/* theme toggle */}
-            </div>
-        </div>
-    </header>
-  )
-}
+  const { theme, setTheme } = useTheme();
+  const isDark = theme === "dark";
 
-export default Header
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-2">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <Link to={"/"}>
+          <img
+            src={isDark ? "/logo.png" : "/logo2.png"}
+            alt="WeatherPro logo"
+            className="h-14"
+          />
+        </Link>
+        <div>
+          {/* search */}
+          {/* theme toggle */}
+          <div
+            onClick={() => setTheme(isDark ? "light" : "dark")}
+            className={`flex items-center cursor-pointer transition-transform duration-500 ${
+              isDark ? "rotate-180" : "rotate-0"
+            }`}
+          >
+            {isDark ? (
+              <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all" />
+            ) : (
+              <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all" />
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
