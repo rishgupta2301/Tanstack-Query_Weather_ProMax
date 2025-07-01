9e9cc455cc8afc87aba8cd96d82fb2e30d5cada5
Rishabh Gupta <guptarishabh1122@gmail.com>
2025-07-01 19:17:29 +0530

    added loading shimmer effect, used js navigator geolocation

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import { AlertTriangle, MapPin, RefreshCw } from "lucide-react";
+import { Button } from "../components/ui/button";
+import { useGeolocation } from "../hooks/use-geolocation";
+import WeatherSkeleton from "../components/loading-skeleton";
+import { Alert, AlertDescription, AlertTitle } from "../components/ui/alert";
 
 const WeatherDashboard = () => {
-  return (
-    <div>WeatherDashboard</div>
-  )
-}
+  const {
+    coordinates,
+    error: locationError,
+    getLocation,
+    isLoading: locationLoading,
+  } = useGeolocation();
 
-export default WeatherDashboard
+  console.log(coordinates);
+
+  const handleRefresh = () => {
+    getLocation();
+
+    if (coordinates) {
+      // reload weather data
+    }
+  };
+
+  if(locationLoading){
+    return <WeatherSkeleton />
+  }
+
+  if(locationError){
+    return (
+      <Alert variant="destructive">
+        <AlertTriangle className="h-4 w-4" />
+        <AlertTitle>Location Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>{locationError}</p>
+          <Button variant="outline" onClick={getLocation} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  if (!coordinates) {
+    return (
+      <Alert>
+        <MapPin className="h-4 w-4" />
+        <AlertTitle>Location Required</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>Please enable location access to see your local weather.</p>
+          <Button variant="outline" onClick={getLocation} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  return (
+    <div className="space-y-4">
+      {/* favorite cities */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">My location</h1>
+        <Button
+          variant="outline"
+          size="icon"
+          onClick={handleRefresh}
+          // disabled={}
+        >
+          <RefreshCw className="h-4 w-4" />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherDashboard;
